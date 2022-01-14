19bdeb52a365fa96fe4fd46d13fdf8f009a75312
SkyMalcolm <sky.lennartsson@iths.se>
2022-01-14 17:05:37 +0100

    Initial Commit

---
+++ TrainingAppRework/TrainingAppReworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAppReworkApp.swift
+//  TrainingAppRework
+//
+//  Created by Malcolm Lennartsson on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAppReworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
