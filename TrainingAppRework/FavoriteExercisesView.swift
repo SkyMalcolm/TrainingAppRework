ea20fb3cd04c84a5940ec86d7f6617230fca2175
SkyMalcolm <sky.lennartsson@iths.se>
2022-01-14 17:20:06 +0100

    first commit

---
+++ TrainingAppRework/FavoriteExercisesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteExercisesView.swift
+//  TrainingAppRework
+//
+//  Created by Malcolm Lennartsson on 2022-01-14.
+//
+
+import SwiftUI
+
+struct FavoriteExercisesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteExercisesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteExercisesView()
+    }
+}
---
+++ TrainingAppRework/MuscleCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  MuscleCategoryView.swift
+//  TrainingAppRework
+//
+//  Created by Malcolm Lennartsson on 2022-01-14.
+//
+
+import SwiftUI
+
+struct MuscleCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MuscleCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MuscleCategoryView()
+    }
+}
