469cdfc6032c73c1509bab5428c1ab8b27b4905c
SkyMalcolm <sky.lennartsson@iths.se>
2022-01-20 15:57:11 +0100

    firebase work

--- TrainingAppRework/TrainingAppRework/MenuView.swift
+++ TrainingAppRework/TrainingAppRework/MenuView.swift
@@ -38,7 +38,7 @@ struct MenuView: View {
 
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
-        MenuView()
+        ContentView()
 
     }
 }
--- TrainingAppRework/TrainingAppRework/TrainingAppReworkApp.swift
+++ TrainingAppRework/TrainingAppRework/TrainingAppReworkApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TrainingAppReworkApp: App {
@@ -15,3 +16,4 @@ struct TrainingAppReworkApp: App {
         }
     }
 }
+
